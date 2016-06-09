4c6154821a5324e23559462c9dc0e47ce668e568
Constantin <constantin.wittig@t-online.de>
2016-06-09 14:30:34 +0200

    Revised log.info messages, so that user gets only important info messages. Removed unused log4j libraries in AvaCore

--- src/com/stt/STTPluginImplementation.java
+++ src/com/stt/STTPluginImplementation.java
@@ -20,9 +20,9 @@ import edu.cmu.sphinx.linguist.g2p.Path;
 
 
 /**
- * The <code>STTPluginImplementation</code> implements the STTPlugin interface of the speech assistant Ava. 
- * The class contains methods to initialize, start, interrupt, continuou and stop the recognizer. 
- * 
+ * The <code>STTPluginImplementation</code> implements the STTPlugin interface of the speech assistant Ava.
+ * The class contains methods to initialize, start, interrupt, continuou and stop the recognizer.
+ *
  * @author Kevin
  * @since 2016-03-27
  * @version 1
@@ -30,32 +30,32 @@ import edu.cmu.sphinx.linguist.g2p.Path;
 public class STTPluginImplementation implements STTPlugin {
 
 	final Logger log = LogManager.getLogger();
-	final int waitForThreadTermination = 500; 
-	
-	private STTWrapper stt; 
-	private Thread asrThread; 
-	
-	private java.nio.file.Path CONFIG_PATH; 
+	final int waitForThreadTermination = 500;
+
+	private STTWrapper stt;
+	private Thread asrThread;
+
+	private java.nio.file.Path CONFIG_PATH;
 	private String CONFIG_NAME = "sttPlugin.properties";
-	
-	private ResultListener listener; 
-	
+
+	private ResultListener listener;
+
 	public STTPluginImplementation() {
-		
-		this.stt = new STTWrapper(); 
-		this.listener = new ResultListener(); 
+
+		this.stt = new STTWrapper();
+		this.listener = new ResultListener();
 		this.stt.setResultListener(this.listener);
-		this.asrThread = new Thread(this.stt); 
+		this.asrThread = new Thread(this.stt);
 		this.asrThread.setName("Sphinx4_Thread");
-		
+
 	}
-	
+
 	@Override
 	public void start() {
-		log.info("Starting the speech recognition plugin.");
-		
+		log.info("Starting CMU Sphinx plugin.");
+
 		log.debug("Loading the properties file for different needed data: " + Data.confDir + Data.confFileName);
-		
+
 		// initialize the config file path
 		try {
 		    java.nio.file.Path basePath = new File(STTPluginImplementation.class.getProtectionDomain().getCodeSource().getLocation().toURI()).toPath().getParent();
@@ -64,40 +64,40 @@ public class STTPluginImplementation implements STTPlugin {
 		    log.error("Error while creating the spotify configuration file path: " + e.getMessage());
 		    log.catching(Level.DEBUG, e);
 		}
-		
-		PropertiesFileLoader fileLoader = new PropertiesFileLoader(CONFIG_PATH); 
+
+		PropertiesFileLoader fileLoader = new PropertiesFileLoader(CONFIG_PATH);
 		if( !fileLoader.readPropertiesFile() )	{
 			log.fatal("Error while loading the STTPlugin properties file. Plugin can't be started.");
-			return; 
+			return;
 		}
-		
+
 		if( !prepareDataObject(fileLoader) )  {
 			log.fatal("Error while loading and validating the property file for the speech recognition plugin.");
 			return;
 		}
-		fileLoader = null; 
-		
+		fileLoader = null;
+
 		if( !this.stt.init(Data.srConf) )
-			return; 
-		
+			return;
+
 		try {
-			
+
 			log.debug("Starting the recognition");
 			// Starting the speech recognition api
 			this.asrThread.start();
 			//this.stt.startRecognition();
-			
+
 		} catch (IllegalThreadStateException e) {
 			log.catching(Level.DEBUG, e);
 			log.fatal(this.asrThread.getName() + " can't be stated. Illegal thread state.");
 			log.fatal("Plugin can't be started.");
 			if( this.stt != null )
-				this.stt = null; 
-			
-			return; 
-		} 
-		
-		//this.stt.startRecognition();		
+				this.stt = null;
+
+			return;
+		}
+
+		//this.stt.startRecognition();
 		// TODO evtl warten, bis sr bereit ist
 	}
 
@@ -106,96 +106,97 @@ public class STTPluginImplementation implements STTPlugin {
 		log.debug("Closing speech recognizer.");
 		this.stt.closeRecognizer();
 		log.info("Speech recognizer closed");
-		
+
 		try {
 			if( this.asrThread != null ) {
 				log.debug("Waiting for termination of " + this.asrThread.getName());
 				this.asrThread.join(this.waitForThreadTermination);
 				log.debug(this.asrThread.getName() + " terminated.");
 			}
-			
+
 			if( this.stt != null ) {
-				this.stt = null; 
+				this.stt = null;
 			}
-			
+
 		} catch (InterruptedException e) {
 			log.catching(Level.DEBUG, e);
-			log.error("Error while waiting for termination of the " + this.asrThread.getName() + " thread."); 
+			log.error("Error while waiting for termination of the " + this.asrThread.getName() + " thread.");
 		}
+		log.info("CMU Sphinx plugin stopped.");
 	}
 
 	@Override
 	public void continueExecution() {
-		log.info("Continuou speech recognition.");
+		log.info("Continuoue speech recognition.");
 		if( this.stt == null ) {
 			log.error("Speech recognition plugin is not started. Can't continuou recognition.");
-			return; 
-		}
-		
-		if( Data.getPluginState() == PluginState.RUNNING ) {
-			log.debug("Can't continuou speech recognition. Recognizer is already running.");
-			return; 
+			return;
 		}
 
-		this.stt.continueRecognition(); 		
+		if( Data.getPluginState() == PluginState.RUNNING ) {
+			log.debug("Can't continuou speech recognition. Recognizer is already running.");
+			return;
+		}
+
+		this.stt.continueRecognition();
 	}
 
 	@Override
 	public void interruptExecution() {
-		log.info("Speech recognizer will be interrupt.");
+		log.info("Speech recognizer will be interrupted.");
 		if( this.stt == null ) {
 			log.error("Speech recognition plugin is not started. Can't interrupt recognition.");
-			return; 
+			return;
 		}
-		
+
 		if( Data.getPluginState() == PluginState.INTERRUPTED ) {
 			log.debug("Can't interrupt speech recognition. Recognizer is already interrupted.");
-			return; 
+			return;
 		}
-		
+
 		Data.setPluginState(PluginState.INTERRUPTED);
 	}
-	
-	private boolean prepareDataObject(PropertiesFileLoader fileLoader) {
-		
-		ArrayList<String> neededProperties = new ArrayList<String>(); 		
-		neededProperties.add("name"); 
-		neededProperties.add("version"); 
-		neededProperties.add("acousticModelPath"); 
-		neededProperties.add("languageModelPath"); 
-		neededProperties.add("dictPath"); 
-		neededProperties.add("CMULogLevel");
-		
-		if( fileLoader.isPropertiesFileValid(neededProperties) != null ) 
-			return false; 
-		
 
-		SRConfigData srData = new SRConfigData(); 
-		srData.acousticModelPath = fileLoader.getPropertie("acousticModelPath"); 
-		srData.languageModelPath = fileLoader.getPropertie("languageModelPath"); 
-		srData.dictionaryPath = fileLoader.getPropertie("dictPath"); 
-		srData.cmuSphinxLogLevel = fileLoader.getPropertie("CMULogLevel"); 
-		
-		Data.srConf = srData; 
-		Data.name = fileLoader.getPropertie("name"); 
-		Data.version = fileLoader.getPropertie("version"); 
-		
-		neededProperties = null; 
-		return true; 
+	private boolean prepareDataObject(PropertiesFileLoader fileLoader) {
+
+		ArrayList<String> neededProperties = new ArrayList<String>();
+		neededProperties.add("name");
+		neededProperties.add("version");
+		neededProperties.add("acousticModelPath");
+		neededProperties.add("languageModelPath");
+		neededProperties.add("dictPath");
+		neededProperties.add("CMULogLevel");
+
+		if( fileLoader.isPropertiesFileValid(neededProperties) != null )
+			return false;
+
+
+		SRConfigData srData = new SRConfigData();
+		srData.acousticModelPath = fileLoader.getPropertie("acousticModelPath");
+		srData.languageModelPath = fileLoader.getPropertie("languageModelPath");
+		srData.dictionaryPath = fileLoader.getPropertie("dictPath");
+		srData.cmuSphinxLogLevel = fileLoader.getPropertie("CMULogLevel");
+
+		Data.srConf = srData;
+		Data.name = fileLoader.getPropertie("name");
+		Data.version = fileLoader.getPropertie("version");
+
+		neededProperties = null;
+		return true;
 	}
 
 	@Override
 	public String requestText() {
 		if( this.asrThread == null || this.stt == null ) {
 			log.error("Speech recognition is not started. You can't recognize a utterance.");
-			return null; 
+			return null;
 		}
 		//log.info("Setting speech recognition sate to 'INTERRUPT' to recognize a requested utterance.");
 		//this.interruptExecution();
-		
+
 		this.stt.setIsRequestedResult(true);
 		//this.continueExecution();
-		String requestedText = this.listener.getResultString();		
+		String requestedText = this.listener.getResultString();
 		log.debug("Requested text is '" + requestedText + "'");
 		return requestedText;
 	}
--- src/com/stt/data/Data.java
+++ src/com/stt/data/Data.java
@@ -6,44 +6,44 @@ import org.ava.pluginengine.PluginState;
 
 
 /**
- * This class includes all data, using by this plugin. 
- * 
+ * This class includes all data, using by this plugin.
+ *
  * @author Kevin
  * @since 2016-03-27
  * @version 1
  */
 public class Data {
-	
-	final static Logger log = LogManager.getLogger(Data.class); 
 
-	private static Data instance = new Data(); 
-	
-	public static String version; 
+	final static Logger log = LogManager.getLogger(Data.class);
+
+	private static Data instance = new Data();
+
+	public static String version;
 	public static  String name;
 	public static String confDir 		= "./res/";  			// TODO �ndern un absoluten pfad
-	public static String confFileName	= "sttPlugin.properties"; 
-	
-	private static PluginState pluginState = null; 
-	
-	public static SRConfigData srConf = null; 
-	
+	public static String confFileName	= "sttPlugin.properties";
+
+	private static PluginState pluginState = null;
+
+	public static SRConfigData srConf = null;
+
 	private Data() {
-		
+
 	}
-	
+
 	public static Data getInstace() {
-		return instance; 
+		return instance;
 	}
-	
+
 	public static PluginState getPluginState() { return pluginState; }
-	
+
 	public static void setPluginState(PluginState state) {
-		pluginState = state; 
-		log.info("Plugin state is set to " + state.toString());
+		pluginState = state;
+		log.debug("Plugin state is set to " + state.toString());
 	}
-	
+
 	public static void setSRConfigData(SRConfigData conf) {
 		if( conf != null )
-			srConf = conf; 
+			srConf = conf;
 	}
 }
